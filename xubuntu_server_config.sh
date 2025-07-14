745a0b641ea39023ea22f8bb6f4fce2b8549e283
Rockyy1990 <56369930+Rockyy1990@users.noreply.github.com>
2025-07-14 10:28:14 +0200

    Add files via upload

--- xubuntu_server_config.sh
+++ xubuntu_server_config.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Last Edit: 14.07.2025
+
 echo ""
 echo "-----------------------------------------"
 echo "  Xubuntu Server  (minimal install)      " 
@@ -11,9 +13,8 @@ set -e
 sudo apt autoremove -y apport apport-gtk
 sudo dpkg --add-architecture i386 
 sudo apt update
+sudo apt upgrade -y
 sudo apt dist-upgrade -y
-sudo apt install --fix-missings
-sudo apt install --fix-broken
 clear
  
 # Add various PPAs
@@ -32,12 +33,12 @@ rm -vrf ~/snap
 sudo rm -vrf /snap /var/snap /var/lib/snapd /var/cache/snapd /usr/lib/snapd 
 sudo apt-mark hold snapd  
 
-
 # Install flatpak 
 sudo apt install -y flatpak 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak update 
 clear
+
 
 echo "Install needed packages.."
 sleep 2
@@ -48,18 +49,23 @@ sudo apt install -y mtools f2fs-tools xfsdump gvfs-backends thunarx-python
 clear
 
 
-wget -qO - https://dl.xanmod.org/archive.key | sudo gpg --dearmor -vo /etc/apt/keyrings/xanmod-archive-keyring.gpg
-echo 'deb [signed-by=/etc/apt/keyrings/xanmod-archive-keyring.gpg] http://deb.xanmod.org releases main' | sudo tee /etc/apt/sources.list.d/xanmod-release.list
-sudo apt update  
-sudo apt install -y linux-xanmod-x64v3
-sudo update-initramfs -u -k all
-sudo update-grub
+#wget -qO - https://dl.xanmod.org/archive.key | sudo gpg --dearmor -vo /etc/apt/keyrings/xanmod-archive-keyring.gpg
+#echo 'deb [signed-by=/etc/apt/keyrings/xanmod-archive-keyring.gpg] http://deb.xanmod.org releases main' | sudo tee /etc/apt/sources.list.d/xanmod-release.list
+#sudo apt update  
+#sudo apt install --install-recommends -y linux-xanmod-x64v3
+#sudo update-initramfs -u -k all
+#sudo update-grub
+
+# Liquorix Kernel
+curl -s 'https://liquorix.net/install-liquorix.sh' | sudo bash
+
 
 
 sudo apt install -y gstreamer1.0-fdkaac gstreamer1.0-plugins-bad gstreamer1.0-plugins-rtp gir1.2-gst-plugins-bad-1.0 gstreamer1.0-espeak 
 sudo apt install -y gstreamer1.0-vaapi gstreamer1.0-pipewire gstreamer1.0-gtk3 rtkit
 sudo apt install -y ffmpeg libavdevice60 libavcodec-extra aac-enc lame libmad0 flac twolame libaacs0 x265 sox libsox-fmt-mp3 libsox-fmt-ao 
-sudo apt install -y celluloid handbrake soundconverter yt-dlp 
+sudo apt install --install-recommends -y celluloid soundconverter yt-dlp 
+
 
 # NoMachine RDP
 # sometimes you need to update the download link and file name
@@ -83,19 +89,23 @@ sudo apt install -y guestfs-tools guestfsd libguestfs-gfs2 libguestfs-rsync pyth
 
 
 
-# Install webmin interface
-curl -o webmin-setup-repo.sh https://raw.githubusercontent.com/webmin/webmin/master/webmin-setup-repo.sh
-sudo sh webmin-setup-repo.sh
-sudo apt-get install --install-recommends webmin 
+# Install Cockpit web interface
+sudo apt install --install-recommends -y cockpit cockpit-networkmanager cockpit-machines
+sudo systemctl enable --now cockpit.socket
 
-sudo systemctl enable webmin
-sudo systemctl start webmin
+# Install webmin interface
+#curl -o webmin-setup-repo.sh https://raw.githubusercontent.com/webmin/webmin/master/webmin-setup-repo.sh
+#sudo sh webmin-setup-repo.sh
+#sudo apt-get install --install-recommends webmin 
+
+#sudo systemctl enable webmin
+#sudo systemctl start webmin
 
 
 # Install nvidia driver
-sudo apt install -y nvidia-dkms-570 nvidia-utils-570  libnvidia-gl-570 libnvidia-gl-570:i386 nvidia-compute-utils-570 libxnvctrl0 nvidia-settings
+sudo apt install --install-recommends -y nvidia-driver-575 nvidia-utils-575  libnvidia-gl-575 libnvidia-gl-575:i386 nvidia-compute-utils-575 libxnvctrl0 nvidia-settings
 sudo apt install -y vulkan-validationlayers libvulkan1 vulkan-tools libosmesa6 mesa-opencl-icd
-sudo update-initramfs -u -k all
+
 
 
 # Enable unattended upgrades for security updates
@@ -114,7 +124,7 @@ sudo systemctl enable libvirtd
 
 # Some needed programs via flatpak
 flatpak install -y flathub com.github.tchx84.Flatseal
-flatpak install -y flathub org.mozilla.firefox
+flatpak install -y flathub com.vivaldi.Vivaldi
 flatpak install -y flathub org.strawberrymusicplayer.strawberry
 flatpak install -y flathub com.warlordsoftwares.formatlab
 flatpak install -y flathub fr.handbrake.ghb
@@ -139,6 +149,7 @@ if command -v ufw >/dev/null 2>&1; then
   sudo ufw allow 10000
   sudo ufw allow 4000/tcp
   sudo ufw allow 4000/udp
+  sudo ufw allow 9090 
   sudo ufw --force enable
 else
   echo "UFW not installed, skipping firewall configuration"
@@ -151,6 +162,7 @@ sudo apt autoremove --purge -y
 sudo apt autoremove -y
 sudo apt autoclean -y
 sudo apt clean
+sudo update-initramfs -u -k all
 sudo update-grub
 
 sudo systemctl enable fstrim.timer  
@@ -159,7 +171,8 @@ sudo fstrim -av
 clear
 echo ""
 echo "Xubuntu server configuration completed successfully."
-echo "You can access Webmin at https://your-server-ip:10000"
+#echo "You can access Webmin at https://your-server-ip:10000"
+echo "You can access Cockpit at https://<your-server-ip>:9090"
 echo ""
 sleep 2
 read -p "Press any key to reboot the server.."
